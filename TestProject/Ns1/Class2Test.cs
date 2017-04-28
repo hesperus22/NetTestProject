70bb478d350eca1c8fe172632eacab3ae04c603f
Przemko Robakowski <przemko@przemko-robakowski.pl>
2017-04-28 13:53:46 +0200

    Additional test

---
+++ TestProject/Ns1/Class2Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using Project.Ns1;
+
+namespace TestProject.Ns1
+{
+    [TestFixture]
+    public class Class2Test
+    {
+        public void TestHello()
+        {
+            new Class2().Hello();
+        }
+    }
+}
